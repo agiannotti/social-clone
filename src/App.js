4837eff073324c80b452b2ee8d33887b171050b6
Anthony Giannotti <aj.gianno@gmail.com>
2021-05-11 19:01:30 -0500

    login component; favicon; logo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import "./App.css";
+import Login from "./components/Login";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Login} />
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
